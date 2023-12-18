0884838361da3547d15502e0ebe9916d5b1bba26
zhushuhao <781783733@qq.com>
2023-12-18 12:57:56 +0800

    add routine_load_consumer

--- be/src/common/config.cpp
+++ be/src/common/config.cpp
@@ -601,7 +601,7 @@ DEFINE_Int32(routine_load_pulsar_timeout_second, "10");
 
 // the size of thread pool for routine load task.
 // this should be larger than FE config 'max_routine_load_task_num_per_be' (default 5)
-DEFINE_Int32(routine_load_thread_pool_size, "10");
+DEFINE_Int32(routine_load_thread_pool_size, "100");
 
 // max external scan cache batch count, means cache max_memory_cache_batch_count * batch_size row
 // default is 20, batch_size's default value is 1024 means 20 * 1024 rows will be cached
@@ -781,7 +781,7 @@ DEFINE_String(kafka_debug, "disable");
 // The number of pool siz of routine load consumer.
 // If you meet the error describe in https://github.com/edenhill/librdkafka/issues/3608
 // Change this size to 0 to fix it temporarily.
-DEFINE_Int32(routine_load_consumer_pool_size, "10");
+DEFINE_Int32(routine_load_consumer_pool_size, "100");
 
 // Used in single-stream-multi-table load. When receive a batch of messages from kafka,
 // if the size of batch is more than this threshold, we will request plans for all related tables.
