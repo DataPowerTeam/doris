1ef6e046782dc03c4769dcdb98483d81814c5e36
zhushuhao <781783733@qq.com>
2023-12-19 00:23:03 +0800

    add judge event_ids

--- be/src/runtime/routine_load/data_consumer_group.h
+++ be/src/runtime/routine_load/data_consumer_group.h
@@ -137,7 +137,7 @@ private:
     // acknowledge pulsar message
     void acknowledge(pulsar::MessageId& message_id, std::string partition);
 
-    bool is_filter_event_ids(std::string data);
+    bool is_filter_event_ids(const char* data);
 
 private:
     // blocking queue to receive msgs from all consumers
