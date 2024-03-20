e25a0b2d59d62fb3003b278f3f301d7b55fc89d4
zhushuhao <781783733@qq.com>
2024-03-20 13:08:29 +0800

    json parse optimize

--- be/src/runtime/routine_load/data_consumer_group.cpp
+++ be/src/runtime/routine_load/data_consumer_group.cpp
@@ -530,9 +530,7 @@ std::vector<std::string> PulsarDataConsumerGroup::convert_rows(std::string& data
                         destination.AddMember(keyName, sourceValue, destination.GetAllocator());
                     } else {
                         rapidjson::Value& object = const_cast<rapidjson::Value&>(array[i]);
-                        rapidjson::Value param(rapidjson::kObjectType);
-                        param.CopyFrom(object, destination.GetAllocator());
-                        std::string eventStruct = convert_map_to_struct(param);
+                        std::string eventStruct = convert_map_to_struct(object);
                         rapidjson::Value eventName("event", destination.GetAllocator());
                         destination.AddMember(eventName, object, destination.GetAllocator());
                         rapidjson::Value eventStructName("event_struct", destination.GetAllocator());
@@ -568,40 +566,38 @@ std::string PulsarDataConsumerGroup::convert_map_to_struct(rapidjson::Value& map
     destination.SetObject();
     if (map.IsObject()) {
         rapidjson::Value timeName("time", destination.GetAllocator());
-        if (map.HasMember("time")) {
-            rapidjson::Value& timeValue = map["time"];
-            destination.AddMember(timeName, timeValue, destination.GetAllocator());
-        } else {
-            rapidjson::Value value;
-            value.SetNull();
-            destination.AddMember(timeName, value, destination.GetAllocator());
-        }
         rapidjson::Value lngName("lng", destination.GetAllocator());
-        if (map.HasMember("lng")) {
-            rapidjson::Value& lngValue = map["lng"];
-            destination.AddMember(lngName, lngValue, destination.GetAllocator());
-        } else {
-            rapidjson::Value value;
-            value.SetNull();
-            destination.AddMember(lngName, value, destination.GetAllocator());
-        }
         rapidjson::Value latName("lat", destination.GetAllocator());
-        if (map.HasMember("lat")) {
-            rapidjson::Value& latValue = map["lat"];
-            destination.AddMember(latName, latValue, destination.GetAllocator());
-        } else {
-            rapidjson::Value value;
-            value.SetNull();
-            destination.AddMember(latName, value, destination.GetAllocator());
-        }
         rapidjson::Value netName("net", destination.GetAllocator());
-        if (map.HasMember("net")) {
-            rapidjson::Value& netValue = map["net"];
-            destination.AddMember(netName, netValue, destination.GetAllocator());
-        } else {
-            rapidjson::Value value;
-            value.SetNull();
-            destination.AddMember(netName, value, destination.GetAllocator());
+        rapidjson::Value value;
+        value.SetNull();
+        destination.AddMember(timeName, value, destination.GetAllocator());
+        destination.AddMember(lngName, value, destination.GetAllocator());
+        destination.AddMember(latName, value, destination.GetAllocator());
+        destination.AddMember(netName, value, destination.GetAllocator());
+        bool time = false;
+        bool lng  = false;
+        bool lat  = false;
+        bool net  = false;
+        for (auto it = map.GetObject().MemberBegin(); it != map.GetObject().MemberEnd(); ++it) {
+            if (time && lng && lat && net) {
+                break;
+            }
+            const auto& member = *it;
+            const char* key = member.name.GetString();
+            if (std::strcmp(key, "time") == 0) {
+                destination["time"].CopyFrom(map["time"], destination.GetAllocator());
+                time = true;
+            } else if (std::strcmp(key, "lng") == 0) {
+                destination["lng"].CopyFrom(map["lng"], destination.GetAllocator());
+                lng = true;
+            } else if (std::strcmp(key, "lat") == 0) {
+                destination["lat"].CopyFrom(map["lat"], destination.GetAllocator());
+                lat = true;
+            } else if (std::strcmp(key, "net") == 0) {
+                destination["net"].CopyFrom(map["net"], destination.GetAllocator());
+                net = true;
+            }
         }
     }
     rapidjson::StringBuffer buffer;
